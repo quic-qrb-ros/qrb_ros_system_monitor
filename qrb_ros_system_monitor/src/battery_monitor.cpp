c2db7c51220f3551598865a90c9656ab3f166a12
Peng Wang <quic_penwang@quicinc.com>
2025-02-24 06:14:49 +0000

    Fix battery monitor sysfs not found
    Signed-off-by: Peng Wang <quic_penwang@quicinc.com>

--- qrb_ros_system_monitor/src/battery_monitor.cpp
+++ qrb_ros_system_monitor/src/battery_monitor.cpp
@@ -13,8 +13,12 @@
 
 using namespace std::chrono_literals;
 
-#define POWER_SUPPLY_SYSFS_PATH "/sys/class/power_supply/"
-#define SOC_ID_SYSFS_PATH "/sys/devices/soc0/soc_id"
+// clang-format off
+const std::string POWER_CAPACITY_SYSFS_PATHS[] = {
+  "/sys/class/power_supply/battery/capacity",
+  "/sys/class/power_supply/qcom-battmgr-bat/capacity",
+};
+// clang-format on
 
 namespace qrb_ros_system_monitor
 {
@@ -23,9 +27,23 @@ BatteryMonitor::BatteryMonitor(const rclcpp::NodeOptions & options)
   : MonitorNode("battery_monitor", options)
 {
   RCLCPP_INFO(this->get_logger(), "Battery Monitor start");
-  pub_ = this->create_publisher<std_msgs::msg::Float32>("battery", 10);
-  timer_ = create_wall_timer(
-      std::chrono::seconds(interval_), std::bind(&BatteryMonitor::on_timer, this));
+
+  bool sysfs_path_exist = false;
+  for (auto const & sysfs_path : POWER_CAPACITY_SYSFS_PATHS) {
+    std::ifstream battery_capacity_file(sysfs_path);
+    if (battery_capacity_file.is_open()) {
+      power_capacity_sysfs_path_ = sysfs_path;
+      sysfs_path_exist = true;
+      break;
+    }
+  }
+  if (!sysfs_path_exist) {
+    RCLCPP_WARN(this->get_logger(), "no power capacity sysfs node found");
+  } else {
+    pub_ = this->create_publisher<std_msgs::msg::Float32>("battery", 10);
+    timer_ = create_wall_timer(
+        std::chrono::seconds(interval_), std::bind(&BatteryMonitor::on_timer, this));
+  }
 }
 
 void BatteryMonitor::on_timer()
@@ -37,35 +55,11 @@ void BatteryMonitor::on_timer()
 
 void BatteryMonitor::get_battery_info(std_msgs::msg::Float32 & info)
 {
-  std::ifstream soc_id_file(SOC_ID_SYSFS_PATH);
-  if (!soc_id_file.is_open()) {
-    RCLCPP_ERROR(this->get_logger(), "open %s error", SOC_ID_SYSFS_PATH);
-    return;
-  }
-
   std::string line;
-  std::getline(soc_id_file, line);
-  soc_id_file.close();
 
-  auto soc_id = -1;
-  std::istringstream iss(line);
-  iss >> soc_id;
-
-  auto battery_sysfs_path = std::string(POWER_SUPPLY_SYSFS_PATH);
-
-  // kona: 455, kailua: 603, qcm6490: 498
-  if (soc_id == 455 || soc_id == 603) {
-    battery_sysfs_path += "battery/capacity";
-  } else if (soc_id == 498) {
-    battery_sysfs_path += "qcom-battmgr-bat/capacity";
-  } else {
-    RCLCPP_ERROR(this->get_logger(), "no battery sysfs found");
-    return;
-  }
-
-  std::ifstream battery_file(battery_sysfs_path);
+  std::ifstream battery_file(power_capacity_sysfs_path_);
   if (!battery_file.is_open()) {
-    RCLCPP_ERROR(this->get_logger(), "open %s error", battery_sysfs_path.c_str());
+    RCLCPP_ERROR(this->get_logger(), "open %s error", power_capacity_sysfs_path_.c_str());
     return;
   }
   line.clear();
